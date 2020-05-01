3483e5ff2821c00f36f1c7f4cafd6bbcd1548002
Hieu Tran <tranhieunz@gmail.com>
2020-05-01 22:07:11 +1200

    initial setup & commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  entry: './client/index.js',
+  mode: 'development',
+  entry: path.join(__dirname, 'client', 'index.js'),
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
-  mode: 'development',
   module: {
     rules: [{
       test: /\.jsx?$/,
@@ -17,6 +18,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new Dotenv()
+  ],
   devtool: 'source-map',
   devServer: {
     contentBase: './server/public'
